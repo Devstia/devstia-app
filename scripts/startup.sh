69fc9b71670ce2bdad9f38bde15a150b27200a4e
Stephen J Carnam <steve@steveorevo.com>
2023-10-04 00:20:25 -0700

    switch to BIOS for xplatform compatibility

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -14,9 +14,8 @@ qemu-system-x86_64 \
         -smp cpus=4,sockets=1,cores=4,threads=1 \
         -m 4G \
         -vga virtio \
+        -bios bios.img \
         -display default,show-cursor=on \
-        -drive if=pflash,format=raw,file=efi_amd64.img,readonly=on \
-        -drive if=pflash,format=raw,file=efi_amd64_vars.img,readonly=on \
         -device virtio-net-pci,netdev=net0 \
         -netdev user,id=net0,hostfwd=tcp::"$sshPort"-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::"$cpPort"-:"$cpPort"$samba \
         -drive if=virtio,format=qcow2,file=pws-amd64.img \
@@ -24,4 +23,6 @@ qemu-system-x86_64 \
         -device virtio-9p-pci,fsdev=virtfs0,mount_tag=appFolder \
         -device virtio-balloon-pci \
         -nographic
+        # -drive if=pflash,format=raw,file=efi_amd64.img,readonly=on \
+        # -drive if=pflash,format=raw,file=efi_amd64_vars.img,readonly=on \
         #-display default,show-cursor=on
