83a23a0a52cb2f10cd2e32f1fbb18329690d93dd
Stephen J. Carnam <steve@steveorevo.com>
2023-10-13 14:44:36 -0700

    windows compatibility

---
+++ scripts/status.bat
@@ -0,0 +1,7 @@
+@echo off
+REM 
+REM This script is executed by the Settings window to obtain
+REM system server status.
+set sshPort=%1
+set private_key=%cd%\..\security\ssh\debian_rsa
+ssh -q -o StrictHostKeyChecking=no -i "%private_key%" debian@local.dev.cc -p %sshPort% "/usr/sbin/service --status-all"
