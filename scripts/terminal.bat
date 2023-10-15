f394edb01baafc319f10ab80d521c455ad984eac
Stephen J Carnam <steve@steveorevo.com>
2023-10-14 17:07:54 -0700

    wip

--- scripts/terminal.bat
+++ scripts/terminal.bat
@@ -4,6 +4,7 @@
 :: icon. It opens a Terminal window and connects to the VM via
 :: SSH on the given port number.
 ::
+title CodeGarden
 set port=%1
 set private_key=%cd%\..\security\ssh\pws_rsa
 ssh -q -o StrictHostKeyChecking=no -i "%private_key%" pws@local.dev.cc -p %port% && exit
