98d1d36c9b990e28b5d119ba9d611d55a62fc0bc
Stephen J Carnam <steve@steveorevo.com>
2023-11-10 17:41:01 -0800

    logo in settings

--- web/links.js
+++ web/links.js
@@ -4,7 +4,7 @@ links.forEach(link => {
     link.addEventListener('click', (event) => {
         if (link.href.startsWith('http://') || link.href.startsWith('https://')) {
             event.preventDefault(); // Prevent the link from opening in the current window
-            ipcMain.send('openLink', link.href);
+            ipcComm.send('openLink', link.href);
         }
     });
 });
