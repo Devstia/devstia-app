2462150064b4025a4ce967934dd49e77a4d3ddd2
Stephen J Carnam <steve@steveorevo.com>
2023-11-15 12:08:55 -0800

    startup adj. for apple silicon

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,23 +7,53 @@ sshPort=$1
 cpPort=$2
 vmsFolder="$3"
 samba=$4
-
-# Set DYLD_FALLBACK_LIBRARY_PATH to the lib directory
-qemu_path=$(which qemu-system-x86_64)
-lib_path=$(dirname "$qemu_path")/../lib
-export DYLD_FALLBACK_LIBRARY_PATH="$lib_path"
-
 cd "$vmsFolder" || exit
-qemu-system-x86_64 \
-        -machine q35,vmport=off -accel hvf \
-        -cpu Haswell-v1 \
-        -smp cpus=4,sockets=1,cores=4,threads=1 \
-        -m 4G \
-        -vga virtio \
-        -bios bios.img \
-        -display default,show-cursor=on \
-        -drive if=virtio,format=qcow2,file=devstia-amd64.img \
-        -device virtio-net-pci,netdev=net0 \
-        -netdev user,id=net0,hostfwd=tcp::$sshPort-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::$cpPort-:$cpPort$samba \
-        -device virtio-balloon-pci \
-        -nographic
+
+# Check if the CPU architecture 
+cpu_arch=$(sysctl -n machdep.cpu.brand_string)
+
+# For Intel-based Macs
+if [[ $cpu_arch == *"Intel"* ]]; then
+
+        # Set DYLD_FALLBACK_LIBRARY_PATH to the lib directory
+        qemu_path=$(which qemu-system-x86_64)
+        lib_path=$(dirname "$qemu_path")/../lib
+        export DYLD_FALLBACK_LIBRARY_PATH="$lib_path"
+
+        qemu-system-x86_64 \
+                -machine q35,vmport=off -accel hvf \
+                -cpu Haswell-v1 \
+                -smp cpus=4,sockets=1,cores=4,threads=1 \
+                -m 4G \
+                -vga virtio \
+                -bios bios.img \
+                -display default,show-cursor=on \
+                -drive if=virtio,format=qcow2,file=devstia-amd64.img \
+                -device virtio-net-pci,netdev=net0 \
+                -netdev user,id=net0,hostfwd=tcp::$sshPort-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::$cpPort-:$cpPort$samba \
+                -device virtio-balloon-pci \
+                -nographic
+fi
+
+# For Apple Silicon-based Macs
+if [[ $cpu_arch == *"Apple M"* ]]; then
+
+        # Set DYLD_FALLBACK_LIBRARY_PATH to the lib directory
+        qemu_path=$(which qemu-system-aarch64)
+        lib_path=$(dirname "$qemu_path")/../lib
+        export DYLD_FALLBACK_LIBRARY_PATH="$lib_path"
+
+        qemu-system-aarch64 \
+                -machine virt -accel hvf \
+                -cpu host \
+                -vga none \
+                -smp cpus=4,sockets=1,cores=4,threads=1 \
+                -m 4G \
+                -drive if=pflash,format=raw,file=efi_arm64.img,file.locking=off,readonly=on \
+                -drive if=pflash,format=raw,file=efi_arm64_vars.img \
+                -device virtio-blk-pci,drive=drivedevstia-arm64,bootindex=0 \
+                -drive if=none,media=disk,id=drivedevstia-arm64,file=devstia-arm64.img,discard=unmap,detect-zeroes=unmap \
+                -device virtio-balloon-pci \
+                -net nic -net user,hostfwd=tcp::8022-:22,hostfwd=tcp::80-:80,hostfwd=tcp::443-:443,hostfwd=tcp::8083-:8083 \
+                -nographic
+fi
