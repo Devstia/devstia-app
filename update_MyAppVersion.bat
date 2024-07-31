a7f8476faa4d7f6645ce55466adecce773f8e9b4
Stephen J. Carnam <steve@steveorevo.com>
2024-07-30 17:15:13 -0700

    Fix version in installer.iss update script

--- update_MyAppVersion.bat
+++ update_MyAppVersion.bat
@@ -2,14 +2,23 @@
 setlocal enabledelayedexpansion
 
 :: Extract version from package.json
-for /f "tokens=4 delims=:, " %%i in ('findstr "version" package.json') do (
+for /f "tokens=2 delims=:, " %%i in ('findstr "\"version\"" package.json') do (
+    echo Found version line: %%i
     set VERSION=%%i
     set VERSION=!VERSION:"=!
+    set VERSION=!VERSION: =!
+    echo Extracted version: !VERSION!
     goto :break
 )
 :break
 
-:: Use PowerShell to replace the version in installer.iss
-PowerShell -Command "(Get-Content installer.iss) -replace '#define MyAppVersion \".*\"', '#define MyAppVersion \"' + '!VERSION!' + '\"' | Set-Content installer.iss"
+:: Check if VERSION is set
+if "%VERSION%"=="" (
+    echo Failed to extract version from package.json
+    exit /b 1
+)
 
-echo Version updated to !VERSION! in installer.iss
+:: Use PowerShell to replace the version in installer.iss
+PowerShell -Command "(Get-Content 'installer.iss') -replace '#define MyAppVersion \".*\"', '#define MyAppVersion \"%VERSION%\"' | Set-Content 'installer.iss'"
+
+echo Version updated to %VERSION% in installer.iss
